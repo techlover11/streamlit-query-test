d04d13671c16645778d2510b85c57bf6e98ba164
techlover11 <41142039+techlover11@users.noreply.github.com>
2024-05-18 20:07:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from streamlit_condition_tree import condition_tree, config_from_dataframe
+
+# Initial dataframe
+df = pd.DataFrame({
+    'First Name': ['Georges', 'Alfred'],
+    'Age': [45, 98],
+    'Favorite Color': ['Green', 'Red'],
+    'Like Tomatoes': [True, False]
+})
+
+# Basic field configuration from dataframe
+config = config_from_dataframe(df)
+
+# Condition tree
+query_string = condition_tree(config)
+
+# Filtered dataframe
+df = df.query(query_string)
